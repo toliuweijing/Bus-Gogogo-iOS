25de6bb733b10825144f57dfca37ae06a9a5725a
Jim Liu <waiging.lau@gmail.com>
2014-06-29 10:06:53 -0400

    copy xibs to storyboard

---
+++ Bus-Gogogo/PTRoutePresenterCell.h
@@ -0,0 +1,15 @@
+//
+//  PTRoutePresenterCell.h
+//  Bus-Gogogo
+//
+//  Created by developer on 6/29/14.
+//  Copyright (c) 2014 Weijing Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTRoutePresenterCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+
+@end
---
+++ Bus-Gogogo/PTRoutePresenterCell.m
@@ -0,0 +1,34 @@
+//
+//  PTRoutePresenterCell.m
+//  Bus-Gogogo
+//
+//  Created by developer on 6/29/14.
+//  Copyright (c) 2014 Weijing Liu. All rights reserved.
+//
+
+#import "PTRoutePresenterCell.h"
+
+@implementation PTRoutePresenterCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
